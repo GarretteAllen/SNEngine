3ac51fa0c545ae41b7d05ea8480ce13c11206d17
Garrette Allen <garrettewallen@icloud.com>
2022-10-23 23:11:04 -0400

    Feat: WorldEdit Base/Undo/Redo/
    Added a base for the future of the world editor inside SNEditor. Added functionality to undo actions/redo actions and removal of scenes. Included a small header file that will facilitate the usage of RelayCommand throughout the project for when adding commands (such as undo/redo)

---
+++ SNEditor/Editors/WorldEditor/WorldEditorView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SNEditor.Editors
+{
+
+    /// <summary>
+    /// Interaction logic for WorldEditorView.xaml
+    /// </summary>
+    public partial class WorldEditorView : UserControl
+    {
+        public WorldEditorView()
+        {
+            InitializeComponent();
+        }
+
+        private void UndoRedoView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- SNEditor/GameProject/OpenProjectView.xaml.cs
+++ SNEditor/GameProject/OpenProjectView.xaml.cs
@@ -11,17 +11,27 @@ namespace SNEditor.GameProject
         public OpenProjectView()
         {
             InitializeComponent();
+
+            // FOCUS FIRST ITEM IN PROJECT LIST
+            Loaded += (s, e) =>
+            {
+                var item = projectsListBox.ItemContainerGenerator
+                .ContainerFromIndex(projectsListBox.SelectedIndex) as ListBoxItem;
+                item?.Focus();
+            };
         }
+        // OPEN PROJECT CALL
         private void OnOpen_Button_Click(object sender, RoutedEventArgs e)
         {
             OpenSelectedProject();
         }
-
+        // DOUBLE CLICK TO OPEN PROJECT
         private void OnListBoxItem_Mouse_DoubleClick(object sender, RoutedEventArgs e)
         {
             OpenSelectedProject();
         }
 
+        // OPEN PROJECT + CLOSE PROJECT BROWSER
         private void OpenSelectedProject()
         {
             var project = OpenProject.Open(projectsListBox.SelectedItem as ProjectData);
